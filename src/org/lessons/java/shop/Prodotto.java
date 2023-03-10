a6766facb61bd3c585d05ec3003f6706629f2c06
Omar-Codess <omartaki8@gmail.com>
2023-03-10 14:35:03 +0100

    Creati campi della classe Prodotto

---
+++ src/org/lessons/java/shop/Prodotto.java
@@ -0,0 +1,10 @@
+package org.lessons.java.shop;
+
+public class Prodotto {
+    //CAMPI
+    private int code;
+    public String name;
+    public String description;
+    protected double price;
+    protected double vat;
+}
