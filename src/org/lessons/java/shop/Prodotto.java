60e33a052f2838fddfc8eb2a8c4bcb035a332874
Omar-Codess <omartaki8@gmail.com>
2023-03-10 14:48:00 +0100

    creato costruttore prodotto

--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -2,9 +2,18 @@ package org.lessons.java.shop;
 
 public class Prodotto {
     //CAMPI
-    private int code;
+    public int code;
     public String name;
     public String description;
     protected double price;
     protected double vat;
+
+    //COSTRUTTORE
+    public Prodotto(){
+        code = 1234;
+        name = "accendino";
+        description = "rosso, ricaricabile, lunga durata";
+        price = 2;
+        vat = 0.50;
+    }
 }
